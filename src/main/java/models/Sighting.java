5841d6c474e8dafd385608bc7ebff746e51ffec7
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-06 02:05:47 +0300

    incoopeorate date and time methods

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,9 +1,20 @@
 package models;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class Sighting {
     private String animalName;
     private int rangerid;
     private String location;
     private Timestamp timestamp;
     private int id;
+
+
+    public Sighting(String animalName, String location, int rangerid) {
+        this.animalName = animalName;
+        this.location = location.trim();
+        this.timestamp = new Timestamp(new Date().getTime());
+        this.rangerid = rangerid;
+    }
 }
