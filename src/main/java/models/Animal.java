9445a91aa674f671c07c71b09f668d1bd921c7c2
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-05 23:38:39 +0300

    declare variables and add getter methods

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -19,4 +19,45 @@ public class Animal {
                 Objects.equals(age, animal.age) &&
                 Objects.equals(type, animal.type);
     }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, health, age, type);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
 }
