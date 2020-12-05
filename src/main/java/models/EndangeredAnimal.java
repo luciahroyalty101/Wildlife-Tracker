7389cf9501a2381d4d1c798c1728791f7dc41614
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-06 01:52:52 +0300

    this . keywords

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -1,4 +1,16 @@
 package models;
 
+import org.sql2o.Connection;
+import java.util.List;
+
 public class EndangeredAnimal {
+    private static final String DB_TYPE = "Endangered";
+
+    public EndangeredAnimal(String name, String health, String age) {
+        this.name = name;
+        this.health = health;
+        this.age = age;
+        this.type = DB_TYPE;
+    }
+
 }
