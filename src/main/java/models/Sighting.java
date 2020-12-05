45a64ce3653b27f27d98764db66f21e864dd4280
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-06 02:02:30 +0300

    use access modifiers

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,4 +1,9 @@
 package models;
 
 public class Sighting {
+    private String animalName;
+    private int rangerid;
+    private String location;
+    private Timestamp timestamp;
+    private int id;
 }
