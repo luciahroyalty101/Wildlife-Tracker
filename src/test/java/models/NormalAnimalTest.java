7590b44345809d82eb90e4bcb6dc9345be89029a
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-06 15:54:54 +0300

    introduce datarule with innerclass

--- src/test/java/models/NormalAnimalTest.java
+++ src/test/java/models/NormalAnimalTest.java
@@ -1,7 +1,17 @@
 package models;
 
+import org.junit.Rule;
+
 import static org.junit.Assert.*;
 
 public class NormalAnimalTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
 
+    private NormalAnimal newAnimal() {
+        return new NormalAnimal("Goat","Healthy","Young");
+    }
+
+    private class DatabaseRule {
+    }
 }
